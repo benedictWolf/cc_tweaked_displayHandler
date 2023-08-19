bed01ae6c77f190630aaaecc6cbdf5be96832396
Caorex <benedictjwolf@gmx.de>
2023-08-19 03:57:48 +0200

    fixed SyntaxErr

--- display1.lua
+++ display1.lua
@@ -1,5 +1,5 @@
 os.loadAPI('display/displayHandler.lua')
 monitor = peripheral.wrap('back')
 monitor.clear()
-Button1 = displayHandler.Button.new(color.black, color.white, 1, 2, 3, 4, 'Test')
+Button1 = displayHandler.Button.new(colors.black, colors.white, 1, 2, 3, 4, 'Test')
 print('This file is used to test the module displayHandler!')
