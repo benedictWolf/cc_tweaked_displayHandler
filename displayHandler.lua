66b0c6c346158957d4f6975864beceb93ce457af
Caorex <benedictjwolf@gmx.de>
2023-08-19 12:07:55 +0200

    -added Labeling

--- displayHandler.lua
+++ displayHandler.lua
@@ -14,9 +14,15 @@ local displayHandler = {}
     Button.__index = self
     function Button.new(color, bgColor, Pos1x, Pos1y, Pos2x, Pos2y, name, monitor)
         local instance = setmetatable({}, self)
+        Button.leftMargin = 0
         Button.name = name
+        Button.width = Pos2x - (Pos1x - 1)
         term.redirect(monitor)
         paintutils.drawFilledBox(Pos1x, Pos1y, Pos2x, Pos2y, bgColor)
+        Button.leftMargin = Button.width - strlen(name) / 2
+        monitor.setCursorPosition(Pos1x - 1 + Button.leftMargin)
+        monitor.setTextColor(color)
+        monitor.write(name)
         return instance
     end
 return displayHandler
