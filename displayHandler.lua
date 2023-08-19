04fab840785e5551a3a316d546bb2cb941380add
Caorex <benedictjwolf@gmx.de>
2023-08-19 12:20:18 +0200

    Changes to Button label

--- displayHandler.lua
+++ displayHandler.lua
@@ -15,12 +15,16 @@ local displayHandler = {}
     function Button.new(color, bgColor, Pos1x, Pos1y, Pos2x, Pos2y, name, monitor)
         local instance = setmetatable({}, self)
         Button.leftMargin = 0
+        Button.topMargin = 0
         Button.name = name
         Button.width = Pos2x - (Pos1x - 1)
+        Button.height = Pos2y - (Pos1y - 1)
+        --Button.labellines = 1
         term.redirect(monitor)
         paintutils.drawFilledBox(Pos1x, Pos1y, Pos2x, Pos2y, bgColor)
         Button.leftMargin = Button.width - string.len(name) / 2
-        monitor.setCursorPos(Pos1x - 1 + Button.leftMargin)
+        Button.topMargin = Button.height / 2
+        monitor.setCursorPos(Pos1x - 1 + Button.leftMargin, Button.topMargin)
         monitor.setTextColor(color)
         monitor.write(name)
         return instance
